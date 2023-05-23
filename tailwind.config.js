9a87bee4cc8786545812d08020fb0a619a067db9
phongDevIT <nemo24112003357@gmail.com>
2023-05-23 10:31:03 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    darkMode: "class",
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                body: ["Epilogue", "sans-serif;"],
+            },
+            colors: {
+                primary: "#1DC071",
+                secondary: "#6F49FD",
+                darkbg: "#3A3A43",
+                darkSecondary: "#1C1C24",
+                softDark: "#22222C",
+                darkSoft: "#24242C",
+                darkStrock: "#3A3A43",
+                darkRef: "#422C32",
+                text2: "#4B5264",
+                text3: "#808191",
+                "icon-color": "#A2A2A8",
+                text5: "#B2B3BD",
+                neutral: "#171725",
+                white: "#ffffff",
+                whiteSoft: "#FCFBFF",
+                graySoft: "#FCFCFC",
+                strock: "#F1F1F3",
+                lite: "#FCFCFD",
+                error: "#EB5757",
+            },
+        },
+    },
+    plugins: [],
+};
